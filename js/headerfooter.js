25e71775adc2c6fd879795620ee94ed076472ad1
DanielPini <danielpini7@hotmail.com>
2024-09-12 15:24:03 +0200

    Added about-conducting, contact and events pages.
    Updated styling for conducting-home page.

--- js/headerfooter.js
+++ js/headerfooter.js
@@ -32,16 +32,30 @@ header.innerHTML = `
 
 footer.innerHTML = `
   <div class="footer">
-    <div class="footer-left footer-div">
-      <div class="footer-socials">
-        <ul class="socials-list">
-          <li class="socials-list-item"><a href="https://www.instagram.com/daniel_pini/"><img src="../assets/icons/instagram_icon.svg" alt="Circle with colours outlining a camera in white."></a></li>
-          <li class="socials-list-item"><a href="https://www.facebook.com/PiniConducts/"><img src="../assets/icons/facebook_icon.svg" alt="Circle with colours outlining a camera in white."></a></li>
-          <li class="socials-list-item"><a href="https://www.youtube.com/@DanielEugenePini"><img src="../assets/icons/youtube_icon.svg" alt="Circle with colours outlining a camera in white."></a></li>
-        </ul>
-      </div>
+    <div id="subscribe">
+      <p>Enter your email to stay up to date on performances and news.</p>
+      <form action="">
+        <input
+          type="email"
+          name="email"
+          id="email" />
+        <input
+          type="submit"
+          value="Submit" />
+      </form>
     </div>
-    <div class="footer-center footer-div">&copy 2024 Daniel Pini</div>
-    <div class="footer-right footer-div"><a href="#">Contact Daniel</a></div>
+    <div id="social-links-copyright-and-contact">
+      <div class="footer-left footer-div">
+        <div class="footer-socials">
+          <ul class="socials-list">
+            <li class="socials-list-item"><a href="https://www.instagram.com/daniel_pini/"><img src="./assets/icons/instagram_icon.svg" alt="Circle with colours outlining a camera in white."></a></li>
+            <li class="socials-list-item"><a href="https://www.facebook.com/PiniConducts/"><img src="./assets/icons/facebook_icon.svg" alt="Circle with colours outlining a camera in white."></a></li>
+            <li class="socials-list-item"><a href="https://www.youtube.com/@DanielEugenePini"><img src="./assets/icons/youtube_icon.svg" alt="Circle with colours outlining a camera in white."></a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="footer-center footer-div">&copy 2024 Daniel Pini</div>
+      <div class="footer-right footer-div"><a href="#">Contact Daniel</a></div>
+    </div>
   </div>`;
 

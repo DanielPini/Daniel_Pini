3ccc40bc89b5fa96edf464041a41956f607b0b89
DanielPini <danielpini7@hotmail.com>
2025-01-21 15:15:42 +1100

    Added click functionality to remove splash from home screen.

--- js/conducting-home.js
+++ js/conducting-home.js
@@ -1,10 +1,16 @@
 const splash = document.querySelector(".conducting");
 
 function removeSplash() {
-  setTimeout(() => {
-    splash.style.opacity = 0;
-    splash.style.pointerEvents = "none";
-  }, 2000);
+  splash.style.opacity = 0;
+  splash.style.pointerEvents = "none";
 }
+setTimeout(() => {
+  if ((splash.style.opacity = 1)) {
+    removeSplash();
+  }
+}, 2000);
 
-removeSplash();
+window.addEventListener("click", () => {
+  removeSplash();
+  window.removeEventListener("click");
+});

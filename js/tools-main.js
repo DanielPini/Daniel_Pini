98663ce8beea4c4928dc5918700ad4ede8da4590
DanielPini <daniel@danielpini.com>
2025-04-20 18:09:03 +0200

    Updated linking of files

--- js/tools-main.js
+++ js/tools-main.js
@@ -1,6 +1,6 @@
-import Metronome from "./Metronome.js";
-import Tapper from "./Tapper.js";
-import tempoDatabase from "./examples.js";
+import Metronome from "/js/Metronome.js";
+import Tapper from "/js/Tapper.js";
+import tempoDatabase from "/js/examples.js";
 const selector = document.querySelector("#toolSelector");
 const toolContainer = document.querySelector(".tool");
 toolContainer.classList.add("metronome-container");
@@ -10,49 +10,51 @@ let currentTool = "metronome";
 let metronome = null;
 let tapper = null;
 checkbox.addEventListener("change", () => {
-    labelText.textContent = checkbox.checked ? "Tapper" : "Metronome";
-    currentTool = checkbox.checked ? "tapper" : "metronome";
-    toolContainer.innerHTML = "";
-    renderTool();
+  labelText.textContent = checkbox.checked ? "Tapper" : "Metronome";
+  currentTool = checkbox.checked ? "tapper" : "metronome";
+  toolContainer.innerHTML = "";
+  renderTool();
 });
-const exampleTableContainer = document.querySelector(".js-example-table-container");
+const exampleTableContainer = document.querySelector(
+  ".js-example-table-container"
+);
 const examplesTable = document.createElement("div");
 examplesTable.classList.add("js-example-table");
 examplesTable.innerHTML = generateTempoTable(tempoDatabase);
-exampleTableContainer === null || exampleTableContainer === void 0 ? void 0 : exampleTableContainer.append(examplesTable);
+exampleTableContainer === null || exampleTableContainer === void 0
+  ? void 0
+  : exampleTableContainer.append(examplesTable);
 // Initial render
 renderTool();
 examplesTable.addEventListener("click", (e) => {
-    const target = e.target;
-    if (!target || !target.dataset || !target.dataset.tempo)
-        return;
-    const tempostr = target.dataset.tempo;
-    const tempo = parseInt(tempostr, 10);
-    if (!isNaN(tempo)) {
-        clickToSetTempo(tempo);
-    }
+  const target = e.target;
+  if (!target || !target.dataset || !target.dataset.tempo) return;
+  const tempostr = target.dataset.tempo;
+  const tempo = parseInt(tempostr, 10);
+  if (!isNaN(tempo)) {
+    clickToSetTempo(tempo);
+  }
 });
 function clickToSetTempo(tempo) {
-    if (currentTool === "metronome") {
-        metronome.setTempo(tempo);
-    }
-    if (currentTool === "tapper") {
-        tapper.setTempo(tempo);
-    }
+  if (currentTool === "metronome") {
+    metronome.setTempo(tempo);
+  }
+  if (currentTool === "tapper") {
+    tapper.setTempo(tempo);
+  }
 }
 function renderTool() {
-    if (currentTool === "metronome") {
-        metronome = new Metronome({ tempo: 120 });
-        metronome.init(toolContainer);
-    }
-    else if (currentTool === "tapper") {
-        const tapButton = toolContainer.querySelector(".js-tapper");
-        tapper = new Tapper({ tempo: 120 });
-        tapper.init(toolContainer);
-    }
+  if (currentTool === "metronome") {
+    metronome = new Metronome({ tempo: 120 });
+    metronome.init(toolContainer);
+  } else if (currentTool === "tapper") {
+    const tapButton = toolContainer.querySelector(".js-tapper");
+    tapper = new Tapper({ tempo: 120 });
+    tapper.init(toolContainer);
+  }
 }
 function generateTempoTable(database) {
-    let table = `<table class="tempo-table">
+  let table = `<table class="tempo-table">
     <thead>
       <tr>
         <th>Tempo</th>
@@ -60,23 +62,23 @@ function generateTempoTable(database) {
       </tr>
     </thead>
     <tbody>`;
-    for (const entry of database) {
-        table += `<tr>
+  for (const entry of database) {
+    table += `<tr>
       <td><strong>${entry.tempoRange}</strong><br/><small>${entry.bpmRange}</small></td>
       <td>
         <strong>Popular:</strong><br/>`;
-        for (const pop of entry.examples.popular) {
-            table += `<span class="tempo-number" data-tempo="${pop.bpm}">${pop.bpm} BPM</span> – <a href="${pop.link}" target="_blank">${pop.title} – ${pop.artist}</a><br/>
+    for (const pop of entry.examples.popular) {
+      table += `<span class="tempo-number" data-tempo="${pop.bpm}">${pop.bpm} BPM</span> – <a href="${pop.link}" target="_blank">${pop.title} – ${pop.artist}</a><br/>
         `;
-        }
-        table += `<br/><strong>Classical:</strong><br/>`;
-        for (const classical of entry.examples.classical) {
-            table += `<span class="js-set-tempo-number tempo-number" data-tempo="${classical.bpm}">${classical.bpm} BPM</span> - <a href="${classical.link}" target="_blank">${classical.title}</a> <br/> 
-      `;
-        }
-        table += `</td></tr>`;
     }
-    table += `</tbody></table>`;
-    return table;
+    table += `<br/><strong>Classical:</strong><br/>`;
+    for (const classical of entry.examples.classical) {
+      table += `<span class="js-set-tempo-number tempo-number" data-tempo="${classical.bpm}">${classical.bpm} BPM</span> - <a href="${classical.link}" target="_blank">${classical.title}</a> <br/> 
+      `;
+    }
+    table += `</td></tr>`;
+  }
+  table += `</tbody></table>`;
+  return table;
 }
 //# sourceMappingURL=tools-main.js.map
